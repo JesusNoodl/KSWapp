b649310a858b50ad0376da51af97b6f838db9fc6
JesusNoodl <maxshallam@gmail.com>
2025-04-18 16:35:14 +0100

    fixing main.py error main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Base
+from models import Base
 from .database import get_db
 
 app = FastAPI()
