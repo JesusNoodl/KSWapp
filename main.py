ccf8f1399063a741511d7345ca131346750186df
JesusNoodl <maxshallam@gmail.com>
2025-04-18 16:42:54 +0100

    Cleaned main.py to fix null byte issue

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base
-from .database import get_db
 
 app = FastAPI()
 
-@app.get("/person/")
-def read_person(db: Session = Depends(get_db)):
-    person = db.query(person).all()
-    return person
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Render!"}
