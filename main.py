87df2f3d7349dfe8036cf19ac34d7708e21bd36e
JesusNoodl <maxshallam@gmail.com>
2025-04-18 16:26:09 +0100

    adding base files for unicorn

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+from .database import get_db
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello from FastAPI"}
+@app.get("/pupils/")
+def read_pupils(db: Session = Depends(get_db)):
+    pupils = db.query(Pupil).all()
+    return pupils
