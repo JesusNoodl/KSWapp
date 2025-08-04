bf1cb14e99dc2ad8bf8ab99a90f5d2bcee43f8ee
Jesus Noodl <maxshallam@gmail.com>
2025-08-04 12:09:42 +0100

    Temp removal create belt in crud so there is no error

--- crud.py
+++ crud.py
@@ -2,9 +2,4 @@ from sqlalchemy.orm import Session
 import models
 import schemas
 
-def create_belt(db: Session, belt: schemas.BeltCreate):
-    db_belt = models.Belt(**belt.model_dump())
-    db.add(db_belt)
-    db.commit()
-    db.refresh(db_belt)
-    return db_belt
+
