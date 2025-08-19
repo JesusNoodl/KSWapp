05579043c2c3f17d0c3c74c5eec6fc40b81d3ba6
Jesus Noodl <maxshallam@gmail.com>
2025-08-19 19:47:23 +0100

    trying to fix location missing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from app import models, schemas, crud
 from app.database import SessionLocal, engine
 from dotenv import load_dotenv
 import os
-from app.api.v1 import belts, promotions, person, class_, age_category, role
+from app.api.v1 import belts, promotions, person, class_, age_category, role, location
 
 load_dotenv()  # Load environment variables from .env
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -28,3 +28,5 @@ app.include_router(person.router, prefix="/person", tags=["Person"])
 app.include_router(class_.router, prefix="/class", tags=["Class"])
 app.include_router(age_category.router, prefix="/age_category", tags=["Age Category"])
 app.include_router(role.router, prefix="/role", tags=["Role"])
+app.include_router(location.router, prefix="/location", tags=["Location"])
+
