fb95b3ea90e8f561a981773491356860f3edeb32
Jesus Noodl <maxshallam@gmail.com>
2025-08-19 16:22:34 +0100

    updated role and age cateory APIV1 to have single get requests

--- app/api/v1/age_category.py
+++ app/api/v1/age_category.py
@@ -1,5 +1,5 @@
 # api/v1/age_category.py
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import crud, schemas, database, models
 
@@ -17,3 +17,11 @@ def get_db():
 @router.get("/", response_model=list[schemas.AgeCategoryOut])
 def get_age_categories(db: Session = Depends(get_db)):
     return db.query(models.AgeCategory).all()
+
+# Get age categories
+@router.get("/{age_categorie}", response_model=schemas.AgeCategoryOut)
+def get_age_categorie(age_categorie: int, db: Session = Depends(get_db)):
+    age_category = db.query(models.AgeCategory).filter(models.AgeCategory.id == age_categorie).first()
+    if age_category is None:
+        raise HTTPException(status_code=404, detail="Age category not found")
+    return age_category
--- app/api/v1/role.py
+++ app/api/v1/role.py
@@ -1,5 +1,5 @@
 # api/v1/role.py
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import crud, schemas, database, models
 
@@ -17,3 +17,11 @@ def get_db():
 @router.get("/", response_model=list[schemas.RoleOut])
 def get_roles(db: Session = Depends(get_db)):
     return db.query(models.Role).all()
+
+# Get role
+@router.get("/{role_id}", response_model=schemas.RoleOut)
+def get_role(role_id: int, db: Session = Depends(get_db)):
+    role = db.query(models.Role).filter(models.Role.id == role_id).first()
+    if role is None:
+        raise HTTPException(status_code=404, detail="Role not found")
+    return role
