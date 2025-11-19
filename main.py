05366c63807180ed17cbc4501f15b78dba281ae1
JesusNoodl <maxshallam@gmail.com>
2025-11-19 16:50:51 +0000

    Added awards to API V1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app import models, schemas, crud
 from app.database import SessionLocal, engine
 from dotenv import load_dotenv
 import os
-from app.api.v1 import belts, promotions, person, class_, age_category, role, location, event, admin, calendar
+from app.api.v1 import belts, promotions, person, class_, age_category, role, location, event, admin, calendar, awards
 
 load_dotenv()  # Load environment variables from .env
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -47,3 +47,4 @@ app.include_router(location.router, prefix="/location", tags=["Location"])
 app.include_router(event.router, prefix="/event", tags=["Event"])
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
 app.include_router(calendar.router, prefix="/calendar", tags=["Calendar"])
+app.include_router(awards.router, prefix="/awards", tags=["Awards"])
