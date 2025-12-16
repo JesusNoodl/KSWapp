a083915766213411184f15df4b82e6808d9b563d
JesusNoodl <maxshallam@gmail.com>
2025-12-16 22:34:23 +0000

    Added get user role API call

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app import models, schemas, crud
 from app.database import SessionLocal, engine
 from dotenv import load_dotenv
 import os
-from app.api.v1 import belts, promotions, person, class_, age_category, role, location, event, admin, calendar, awards, contact
+from app.api.v1 import belts, promotions, person, class_, age_category, role, location, event, admin, calendar, awards, contact, user
 
 load_dotenv()  # Load environment variables from .env
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -49,3 +49,4 @@ app.include_router(admin.router, prefix="/admin", tags=["Admin"])
 app.include_router(calendar.router, prefix="/calendar", tags=["Calendar"])
 app.include_router(awards.router, prefix="/awards", tags=["Awards"])
 app.include_router(contact.router, prefix="/contact", tags=["Contact"])
+app.include_router(user.router, prefix="/user", tags=["User"])
